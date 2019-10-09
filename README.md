194f14565d02e32fe23214dbd3d17c973d8635f6
Tyler Long <tyler4long@gmail.com>
2019-10-09 14:31:22 -0700

    Create RestClient struct

---
+++ README.md
@@ -0,0 +1,1 @@
+# RingCentral GoLang SDK
--- ringcentral.go
+++ ringcentral.go
@@ -1,7 +1,21 @@
 package ringcentral
 
+import "net/http"
+
 // ProductionServer RingCentral production server url
 const ProductionServer = "https://platform.ringcentral.com"
 
 // SandboxServer RingCentral sandbox server url
 const SandboxServer = "https://platform.devtest.ringcentral.com"
+
+// RestClient RingCentral rest client
+type RestClient struct {
+	ClientID     string
+	ClientSecret string
+	Server       string
+}
+
+// Get HTTP GET
+func (rc RestClient) Get(endpoint string) (*http.Response, error) {
+	return http.Get(rc.Server + endpoint)
+}
