bb49a319ea4ecfb709abb42f7c24b725058c55e2
Tyler Long <tyler4long@gmail.com>
2019-10-09 14:05:13 -0700

    Initial commit

---
+++ ringcentral.go
@@ -0,0 +1,7 @@
+package ringcentral
+
+// ProductionServer RingCentral production server url
+const ProductionServer = "https://platform.ringcentral.com"
+
+// SandboxServer RingCentral sandbox server url
+const SandboxServer = "https://platform.devtest.ringcentral.com"
