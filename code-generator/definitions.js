dd74cac77d392ea1352888977f7b0cc7a7e5c6b3
Tyler Long <tyler4long@gmail.com>
2019-10-10 15:11:17 -0700

    Generate TokenInfo

--- code-generator/definitions.js
+++ code-generator/definitions.js
@@ -1,5 +1,8 @@
 import yaml from 'js-yaml'
 import fs from 'fs'
+import pascalCase from 'pascal-case'
+import toStartCase from 'to-start-case'
+import path from 'path'
 
 const outputDir = '../definitions'
 
@@ -7,6 +10,25 @@ const doc = yaml.safeLoad(fs.readFileSync('/Users/tyler.liu/src/dotnet/RingCentr
 const definitions = doc.definitions
 const models = Object.keys(definitions).map(k => ({ name: k, ...definitions[k] })).filter(m => m.name === 'TokenInfo')
 
+const normalizeType = type => {
+  switch (type) {
+    case 'integer':
+      return 'int'
+    default:
+      return type
+  }
+}
+
 for (const model of models) {
-  console.log(outputDir, model)
+  const code = `package definitions
+
+// ${model.name} ${toStartCase(model.name)}
+type ${model.name} struct {
+${
+  Object.keys(model.properties).map(k => ({ name: k, ...model.properties[k] }))
+    .map(p => `\t${pascalCase(p.name)} ${normalizeType(p.type)} \`json:"${p.name}"\``).join('\n')
+}
+}
+`
+  fs.writeFileSync(path.join(outputDir, `${model.name.toLowerCase()}.go`), code)
 }
