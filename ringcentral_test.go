0b189618fa78168f56fea3d46910424e3db8f384
Tyler Long <tyler4long@gmail.com>
2021-02-18 08:20:01 -0800

    It killed my program

--- ringcentral_test.go
+++ ringcentral_test.go
@@ -1,7 +1,9 @@
 package ringcentral
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -28,4 +30,28 @@ func TestAuthorize(t *testing.T) {
 	if rc.Token != nil {
 		t.Error("Revoke failed")
 	}
+}
+
+func TestAPICall(t *testing.T) {
+	rc := RestClient{
+		ClientID:     os.Getenv("RINGCENTRAL_CLIENT_ID"),
+		ClientSecret: os.Getenv("RINGCENTRAL_CLIENT_SECRET"),
+		Server:       os.Getenv("RINGCENTRAL_SERVER_URL"),
+	}
+
+	rc.Authorize(GetTokenRequest{
+		GrantType: "password",
+		Username:  os.Getenv("RINGCENTRAL_USERNAME"),
+		Extension: os.Getenv("RINGCENTRAL_EXTENSION"),
+		Password:  os.Getenv("RINGCENTRAL_PASSWORD"),
+	})
+
+	bytes := rc.Post("/restapi/v1.0/client-info/sip-provision", strings.NewReader(`{"sipInfo":[{"transport":"WSS"}]}`))
+	// fmt.Println(bytes)
+	fmt.Println(len(bytes))
+	// if len(bytes) <= 0 {
+	// 	t.Error("Response is empty")
+	// }
+
+	rc.Revoke()
 }
